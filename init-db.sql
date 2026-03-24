8aa3a637ac760f443a52bbd1e897ca78c0f94211
Pedro Rico <67664223+DarkermanDiaz@users.noreply.github.com>
2026-03-23 20:02:36 -0600

    Modificacion para MySQL

--- init-db.sql
+++ init-db.sql
@@ -1,10 +1,8 @@
--- Grant privileges to the credit_user on credit_db
--- This script is executed by postgres user, after credit_db is created via POSTGRES_DB env var
-GRANT ALL PRIVILEGES ON DATABASE credit_db TO credit_user;
-GRANT USAGE ON SCHEMA public TO credit_user;
-GRANT CREATE ON SCHEMA public TO credit_user;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO credit_user;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO credit_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO credit_user;
+-- Initialize MySQL database for Credit Application
+-- This script is executed after MySQL container starts
+
+-- Grant privileges to credit_user on credit_db
+GRANT ALL PRIVILEGES ON credit_db.* TO 'credit_user'@'%';
+FLUSH PRIVILEGES;
 
 
